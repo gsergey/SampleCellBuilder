a1423ab6f5a7157ba8d298ad7484995d1bbab911
Sergey Galagan <galaganss@gmail.com>
2022-08-29 23:58:13 +0300

    - init project;

---
+++ SampleCellBuilder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleCellBuilder
+//
+//  Created by Sergey on 29.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
